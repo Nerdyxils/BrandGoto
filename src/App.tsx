e8479fc6c0893683eacf4505f976b11890d77147
Nerdyxils <silasabiodun25@gmail.com>
2025-06-16 02:36:36 -0400

    undo any QR code issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Preloader from './components/Preloader';
 import Hero from './components/Hero';
 import BusinessAssessmentLanding from './complete-business-assessment/business-assessment.tsx';
-// import Qr from './assets/qr/qr-code.tsx';
 
 const App: React.FC = () => {
   const [isPreloaderComplete, setIsPreloaderComplete] = useState(false);
@@ -16,7 +15,6 @@ const App: React.FC = () => {
           <Routes>
             <Route path="/" element={<Hero />} />
             <Route path="/complete-business-assessment" element={<BusinessAssessmentLanding />} />
-            {/* <Route path="/qr" element={<Qr />} /> */}
           </Routes>
         </Router>
       )}
